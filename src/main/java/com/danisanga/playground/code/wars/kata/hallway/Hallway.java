6e4355177483f333451e3eed98d15ef8ae4427be
Dani Sanchez <sanchez.daniel.dev@gmail.com>
2023-08-20 21:02:55 +0200

    [DS] WIP 01

--- src/main/java/com/danisanga/playground/code/wars/kata/hallway/Hallway.java
+++ src/main/java/com/danisanga/playground/code/wars/kata/hallway/Hallway.java
@@ -1,8 +1,10 @@
 package com.danisanga.playground.code.wars.kata.hallway;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 /**
  * Code Wars <a href="https://www.codewars.com/kata/6368426ec94f16a1e7e137fc/java">kata</a>.
@@ -10,32 +12,75 @@ import java.util.Map;
 public class Hallway {
 
     public static int contact(String hallway) {
-
         // Checks if hallway String is valid... If it does not contain">" or "<" no people will contact each other.
         if (!hallway.contains(">") && !hallway.contains("<")) {
             return -1;
         }
 
         // Gets characters from hallway.
-        final List<Character> characters = hallway.chars().mapToObj(e -> (char) e).toList();
-        System.out.println(characters);
+        final List<String> characters = separateCharacters(hallway);
 
-        // Search for coincidences... This means get the number of "-" between people.
-        // TODO - WIP
-        Map<String, Integer> coincidences = new HashMap<>();
-        for (int i = 0; i < characters.size(); i++) {
-            if (characters.get(i).equals('>')) {
-                int numberOfHyphens = 0;
-                while (i < characters.size() && !characters.get(i).equals('<')) {
-                    numberOfHyphens++;
-                    i++;
+        Map<String, Integer> result = countDashesBetweenMarkers(characters);
+        result.forEach((key, value) -> System.out.println(key + ": " + value));
+
+        if (result.isEmpty()) {
+            return -1;
+        } else {
+            Map.Entry<String, Integer> minValue = getMinValue(result);
+            return Objects.nonNull(minValue) ? minValue.getValue() : -1;
+        }
+    }
+
+    private static Map<String, Integer> countDashesBetweenMarkers(List<String> characters) {
+        Map<String, Integer> resultMap = new LinkedHashMap<>(); // LinkedHashMap mantiene el orden de inserción
+        List<Integer> markerPositions = new ArrayList<>();
+
+        // Recoger las posiciones de '>' y '<'
+        IntStream.range(0, characters.size()).forEach(i -> {
+            if (characters.get(i).contains(">") || characters.get(i).contains("<")) {
+                markerPositions.add(i);
+            }
+        });
+
+        // Asegurarse de que las posiciones de '>' y '<' son correctas
+        for (int i = 0; i < markerPositions.size() - 1; i += 2) {
+            if (characters.get(markerPositions.get(i)).contains(">") && characters.get(markerPositions.get(i + 1)).contains("<")) {
+                int start = markerPositions.get(i) + 1;
+                int end = markerPositions.get(i + 1);
+                int count = (int) characters.subList(start, end).stream().filter(ch -> ch.equals("-")).count();
+                if (count == 0) {
+                    count = 1;
                 }
-                coincidences.put(">", numberOfHyphens);
-                break;
+                resultMap.put(String.valueOf((char) ('a' + (i / 2))), count); // Convertir el índice a letra del abecedario
+            }
+        }
+        return resultMap;
+    }
+
+    private static Map.Entry<String, Integer> getMinValue(Map<String, Integer> resultMap) {
+        return resultMap.entrySet().stream().min(Map.Entry.comparingByValue()).orElse(null);
+    }
+
+    private static List<String> separateCharacters(final String hallway) {
+        List<String> result = new ArrayList<>();
+
+        // Expresión regular para capturar secuencias de '>' y '<'
+        Pattern pattern = Pattern.compile("([>]+|[<]+|-)");
+        Matcher matcher = pattern.matcher(hallway);
+
+        while (matcher.find()) {
+            String match = matcher.group();
+
+            // Si es '-' lo agregamos directamente
+            if (match.equals("-")) {
+                result.add(match);
+            } else {
+                // Si es una secuencia de '>' o '<', agregamos la secuencia completa
+                result.add(match);
             }
         }
 
-        // Returns expected result.
-        return coincidences.get(">");
+        System.out.println(result);
+        return result;
     }
 }
